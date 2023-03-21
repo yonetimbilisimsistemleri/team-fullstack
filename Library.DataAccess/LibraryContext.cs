215b3e9340187a3e674f3133054b062d47acbc95
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-03-21 20:01:35 +0300

    Backend init

---
+++ Library.DataAccess/LibraryContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DataAccess
+{
+    public class LibraryContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //TODO: Add connection string here:
+        }
+
+        //TODO: Add tables
+    }
+}
